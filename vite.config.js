ce5fffbf15a684cddd226bef2809116e70332559
kevin beier <kabeier@gmail.com>
2026-07-22 19:15:09 -0400

    conditional base in viteconfig

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,11 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
-export default defineConfig({
+export default defineConfig(({mode})=>({
    base: mode === "production" ? "/demo-test/" : "/",
   plugins: [react(), tailwindcss()],
     server: {
     allowedHosts: [
     ],
   },
-})
+}))
